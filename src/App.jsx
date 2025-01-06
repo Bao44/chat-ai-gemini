eb4cbc8686ac9a391124898584c2ebb9d51c8446
Bao44 <tqbao44@gmail.com>
2025-01-06 21:24:49 +0700

    get code gemini and test

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import { Outlet } from "react-router-dom";
 import SideBar from "./components/SideBar";
+import { useEffect } from "react";
+import Gemini from "./gemini";
 
 function App() {
+
+  useEffect(() => {
+    Gemini('JS là gì');
+  },[])
+
   return (
     <>
       <div className="bg-primaryBg-default h-screen flex">
---
+++ src/gemini/index.js
@@ -0,0 +1,46 @@
+import {
+    GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold
+} from "@google/generative-ai";
+
+const apiKey = import.meta.env.VITE_GOOGLE_API_KEY;
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-2.0-flash-exp",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 40,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+const safetySetting = [
+    {
+        category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+        threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+    },
+    {
+        category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+        threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+    }
+];
+
+async function run(textInput) {
+  const chatSession = model.startChat({
+    generationConfig,
+    safetySetting,
+    history: [
+      
+    ],
+  });
+
+  const result = await chatSession.sendMessage(textInput);
+  console.log(result.response.text());
+}
+
+export default run;
